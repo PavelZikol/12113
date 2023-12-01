56a9333194a2b974e0b4ceec16e87ff0e4f4c13b
PavelZikol <142353611+PavelZikol@users.noreply.github.com>
2023-12-01 23:31:43 +0400

    1

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from catalog.forms import ContactForm
+
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
+        print(name, phone, message)
+    return render(request, 'catalog/contacts.html')
